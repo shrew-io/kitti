4c27b91b69bc0dc586f3e71f5e8ab8490c78089f
Kevin Schmitt <ks@shrew.io>
2014-11-30 16:49:45 +0100

    Changed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
 var install = require('gulp-install');
 var WebkitBuilder = require('node-webkit-builder');
 var git = require('git-rev');
@@ -7,21 +6,45 @@ var fsx = require('fs-extra');
 var npm = require("npm");
 var os = require('os');
 var q = require('q');
-var date = Date.now();
-var temp = 'build/temp';
+var recess = require('recess');
 var manifest = require('./package.json');
+var temp = 'build/temp';
+
+/* Helper
+ ****************** */
+
+var copy = function copy() {
+	var deferred = q.defer();
+
+	fsx.removeSync(temp);
+	fsx.copySync('./src', temp + '/');
+	fsx.removeSync(temp + '/less');
+
+	recess(['./src/less/style.less'], {
+		compile: true
+	}, function (err, obj) {
+		if (err) {
+			throw err;
+		}
+
+		fsx.outputFileSync(temp + '/assets/css/style.css', obj.data);
+		deferred.resolve();
+	});
+
+	return deferred.promise;
+};
 
 var getBuild = function getBuild() {
-    var deferred = q.defer();
-    git.short(function (rev) {
-        var name = manifest.name + '-v' + manifest.version + '.' + rev;
-        deferred.resolve({
-            appName: manifest.name,
-            name: name,
-            path: 'build/' + name
-        });
-    });
-    return deferred.promise;
+	var deferred = q.defer();
+	git.short(function (rev) {
+		var name = manifest.name + '-v' + manifest.version + '.' + rev;
+		deferred.resolve({
+			appName: manifest.name,
+			name: name,
+			path: 'build/' + name
+		});
+	});
+	return deferred.promise;
 };
 
 /* Chained tasks
@@ -37,157 +60,151 @@ gulp.task('default', ['build', 'package']);
  ****************** */
 
 gulp.task('copy', function () {
-    console.log(manifest);
-
-    fsx.removeSync(temp);
-    fsx.copySync('./src', temp + '/');
-    fsx.removeSync(temp + '/less');
+	return copy();
 });
 
-gulp.task('prepare-run', ['copy', 'less', 'dependencies'], function () {
-    manifest.window.toolbar = true;
-    fsx.writeJsonSync(temp + '/package.json', manifest);
+gulp.task('prepare-run', ['copy', 'dependencies'], function () {
+	manifest.window.toolbar = true;
+	fsx.writeJsonSync(temp + '/package.json', manifest);
 });
 
-gulp.task('prepare-build', ['copy', 'less', 'dependencies'], function () {
-    fsx.writeJsonSync(temp + '/package.json', manifest);
+gulp.task('prepare-build', ['copy', 'dependencies'], function () {
+	fsx.writeJsonSync(temp + '/package.json', manifest);
 });
 
 gulp.task('dependencies', ['copy'], function () {
-    return gulp.src(['bower.json', 'package.json'])
-        .pipe(gulp.dest(temp))
-        .pipe(install({
-            production: true
-        }));
-});
-
-gulp.task('less', ['copy'], function () {
-    return gulp.src('src/less/**/style.less')
-        .pipe(less())
-        .pipe(gulp.dest(temp + '/assets/css'));
+	return gulp.src(['bower.json', 'package.json'])
+		.pipe(gulp.dest(temp))
+		.pipe(install({
+			production: true
+		}));
 });
 
 /* Execute / Build
  ****************** */
 
 gulp.task('webkit-run', ['prepare-run'], function () {
-    var nw = new WebkitBuilder({
-        files: temp + '/**'
-    });
-    nw.run();
+	var deferred = q.defer();
+
+	var nw = new WebkitBuilder({
+		files: temp + '/**'
+	});
+
+	return nw.run(function () {
+		console.log('App: Exited');
+	});
 });
 
 gulp.task('webkit-build', ['prepare-build'], function () {
-    var deferred = q.defer();
+	var deferred = q.defer();
 
-    getBuild().then(function (build) {
-        fsx.removeSync(build.path);
+	getBuild().then(function (build) {
+		fsx.removeSync(build.path);
 
-        var nw = new WebkitBuilder({
-            files: temp + '/**',
-            platforms: ['win', 'osx'],
-            macIcns: './src/assets/images/icons/application.icns',
-            winIco: './src/assets/images/icons/application.ico',
-            buildType: function () {
-                return build.name;
-            }
-        });
+		var nw = new WebkitBuilder({
+			files: temp + '/**',
+			platforms: ['win', 'osx'],
+			macIcns: './src/assets/images/icons/application.icns',
+			winIco: './src/assets/images/icons/application.ico',
+			buildType: function () {
+				return build.name;
+			}
+		});
 
-        nw.build().then(function () {
-            deferred.resolve();
-        }).catch(function (error) {
-            console.error('WebKit Error: %s', error);
-            throw {
-                error: 'WebKit',
-                message: error
-            }
-        });
-    });
+		nw.build().then(function () {
+			deferred.resolve();
+		}).catch(function (error) {
+			console.error('WebKit Error: %s', error);
+			throw {
+				error: 'WebKit',
+				message: error
+			}
+		});
+	});
 
-    return deferred.promise;
+	return deferred.promise;
 });
 
-/* Packaging
+/* Package
  ****************** */
 
 gulp.task('package-windows', ['build'], function () {
-    var deferred = q.defer();
+	var deferred = q.defer();
 
-    getBuild().then(function (build) {
-        // TODO
-        //console.log('Created: %s', zipFile);
-    });
+	getBuild().then(function (build) {
+		// TODO
+	});
 
-    return deferred.promise;
+	return deferred.promise;
 });
 
 gulp.task('package-osx', ['build'], function () {
-    var isOsx = os.type() == 'Darwin';
-    if (!isOsx) {
-        console.log('Warning: Skipping OSX build');
-        return;
-    }
+	var isOsx = os.type() == 'Darwin';
+	if (!isOsx) {
+		console.log('Warning: Skipping OSX build');
+		return;
+	}
 
-    var deferred = q.defer();
+	var deferred = q.defer();
 
-    npm.load(manifest, function (err) {
-        npm.commands.install(["appdmg"], function (error, data) {
-            if (error) {
-                throw error;
-            }
+	npm.load(manifest, function (err) {
+		npm.commands.install(["appdmg"], function (error, data) {
+			if (error) {
+				throw error;
+			}
 
-            var appdmg = require('appdmg');
-            getBuild().then(function (build) {
-                fsx.copySync('dist/background.png', build.path + '/background.png');
+			var appdmg = require('appdmg');
+			getBuild().then(function (build) {
+				fsx.copySync('dist/background.png', build.path + '/background.png');
 
-                var appName = build.appName;
-                var fileName = 'osx/' + build.appName + '.app';
-                var osx = {
-                    'title': appName,
-                    'icon': 'assets/images/icons/application.icns',
-                    'background': 'background.png',
-                    'icon-size': 80,
-                    'contents': [{
-                        'x': 100,
-                        'y': 100,
-                        'type': 'link',
-                        'path': '/Applications'
-            }, {
-                        'x': 250,
-                        'y': 100,
-                        'type': 'file',
-                        'path': fileName
-            }]
-                };
+				var appName = build.appName;
+				var fileName = 'osx/' + build.appName + '.app';
+				var osx = {
+					'title': appName,
+					'icon': 'assets/images/icons/application.icns',
+					'background': 'background.png',
+					'icon-size': 80,
+					'contents': [{
+						'x': 100,
+						'y': 100,
+						'type': 'link',
+						'path': '/Applications'
+            			}, {
+						'x': 250,
+						'y': 100,
+						'type': 'file',
+						'path': fileName
+            		}]
+				};
 
-                fsx.writeJsonSync(build.path + '/osx.json', osx);
+				fsx.writeJsonSync(build.path + '/osx.json', osx);
 
-                var dmgFile = build.name + '.dmg';
-                var dmg = appdmg(build.path + '/osx.json', build.path + '/' + dmgFile);
+				var dmgFile = build.name + '.dmg';
+				var dmg = appdmg(build.path + '/osx.json', build.path + '/' + dmgFile);
 
-                var removeTemp = function () {
-                    fsx.removeSync(build.path + '/osx.json');
-                    fsx.removeSync(build.path + '/background.png');
-                };
+				var removeTemp = function () {
+					fsx.removeSync(build.path + '/osx.json');
+					fsx.removeSync(build.path + '/background.png');
+				};
 
-                dmg.on('finish', function () {
-                    console.log('Created: %s', dmgFile);
-                    removeTemp();
-                    deferred.resolve();
-                });
+				dmg.on('finish', function () {
+					console.log('Created: %s', dmgFile);
+					removeTemp();
+					deferred.resolve();
+				});
 
-                dmg.on('error', function (error) {
-                    console.error('DMG Error: %s', error);
-                    removeTemp();
-                    deferred.reject();
-                });
-            });
-        });
+				dmg.on('error', function (error) {
+					console.error('DMG Error: %s', error);
+					removeTemp();
+					deferred.reject();
+				});
+			});
+		});
 
-        npm.on("log", function (message) {
-            console.log(message);
-        });
-    });
+		npm.on("log", function (message) {
+			console.log(message);
+		});
+	});
 
-    return deferred.promise;
+	return deferred.promise;
 });
